b07141ec221102c0e3a245eb9e62ef874085fbb8
Joonas Kuorilehto <joneskoo@derbian.fi>
2018-03-09 22:30:23 +0200

    import: actually import data to server

--- cmd/import.go
+++ cmd/import.go
@@ -1,15 +1,20 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/joneskoo/mymonies/pkg/datasource"
 	"github.com/joneskoo/mymonies/pkg/datasource/nordea/pdf"
 	"github.com/joneskoo/mymonies/pkg/datasource/nordea/tsv"
+	"github.com/joneskoo/mymonies/pkg/rpc/mymonies"
 	"github.com/spf13/cobra"
 )
+
+var serverAddress string
 
 // importCmd represents the import command
 var importCmd = &cobra.Command{
@@ -19,27 +24,24 @@ var importCmd = &cobra.Command{
 	to a mymonies server.`,
 	Args: requiredFilesWithTypes(".txt", ".pdf"),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		_, err := readFiles(args)
-		if err != nil {
-			return err
+		ctx := context.Background()
+		client := mymonies.NewMymoniesProtobufClient(serverAddress, &http.Client{})
+		for _, filename := range args {
+			f, err := parseFile(filename)
+			if err != nil {
+				return fmt.Errorf("%v: %v", filename, err)
+			}
+			_, err = client.AddImport(ctx, &mymonies.AddImportReq{
+				Account:      f.Account(),
+				FileName:     f.FileName(),
+				Transactions: f.Transactions(),
+			})
+			if err != nil {
+				return fmt.Errorf("%v: %v", filename, err)
+			}
+			fmt.Println(f.FileName(), len(f.Transactions()), "transactions")
 		}
 		return nil
-		// ctx := context.Background()
-		// client := mymonies.NewMymoniesProtobufClient(*server, &http.Client{})
-		// _, err = client.AddImport(ctx, &mymonies.AddImportReq{
-		// 	Account:      file.Account(),
-		// 	FileName:     file.FileName(),
-		// 	Transactions: file.Transactions(),
-		// })
-		// if err != nil {
-		// 	logger.Fatalf("%v: %v", filename, err)
-		// }
-		// // fmt.Printf("Account: %q File: %q\n", f.Account(), f.FileName())
-		// // for _, tx := range f.Transactions() {
-		// // 	fmt.Printf("%v %12v %-40v %8.2f %v\n", tx.TransactionDate.Format("02.01.2006"), tx.Transaction, tx.PayeePayer, tx.Amount, tx.Tag)
-		// // }
-		// // fmt.Println("---------------------------")
-
 	},
 }
 
@@ -65,19 +67,6 @@ func requiredFilesWithTypes(types ...string) cobra.PositionalArgs {
 	}
 }
 
-func readFiles(args []string) ([]datasource.File, error) {
-	files := make([]datasource.File, 0)
-	for _, filename := range args {
-		f, err := parseFile(filename)
-		if err != nil {
-			return nil, fmt.Errorf("%v: %v", filename, err)
-		}
-		files = append(files, f)
-		fmt.Println(filename)
-	}
-	return files, nil
-}
-
 func parseFile(filename string) (datasource.File, error) {
 	ext := filepath.Ext(filename)
 	switch ext {
@@ -96,6 +85,6 @@ func parseFile(filename string) (datasource.File, error) {
 func init() {
 	rootCmd.AddCommand(importCmd)
 
-	importCmd.PersistentFlags().String("mymonies", "http://127.0.0.1:8000", "Store imported transactions to mymonies server")
-	importCmd.PersistentFlags().String("json", "", "Output imported data as JSON files into directory")
+	importCmd.PersistentFlags().StringVar(&serverAddress, "mymonies", "http://127.0.0.1:8000", "Store imported transactions to mymonies server")
+	// importCmd.PersistentFlags().String("json", "", "Output imported data as JSON files into directory")
 }
